928f4f9e24ce541c6dbc9975ed873865ce9a7bb4
Tatyana Korosteleva <tvkorosteleva@avito.ru>
2016-11-01 15:33:19 +0300

    #! add fallback for background-email

--- projects/background-element/index.js
+++ projects/background-element/index.js
@@ -1,23 +1,23 @@
 (function () {
-  var asideNode = document.querySelector('.aside');
-  if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    var asideNode = document.querySelector('.aside');
     var asideDelimetrNode = document.querySelector('.delimetr');
     var documentHeight = document.body.clientHeight;
 
     function setDelimetrHeight() {
-      var visibleClientHeight = window.innerHeight;
-      var asideHeight = visibleClientHeight * parseInt(getComputedStyle(asideNode).height) / documentHeight;
-      asideDelimetrNode.style.setProperty('--height', asideHeight + 'px');
+        var visibleClientHeight = window.innerHeight;
+        var asideHeight = visibleClientHeight * parseInt(getComputedStyle(asideNode).height) / documentHeight;
+        asideDelimetrNode.style.setProperty('--height', asideHeight + 'px');
+    }
+
+    function getScrolledTop() {
+        return document.documentElement.scrollTop || document.body.scrollTop;
     }
     setDelimetrHeight();
     window.addEventListener('resize', setDelimetrHeight);
 
     window.addEventListener('scroll', function () {
-      var scrolledFromTop = document.documentElement.scrollTop * 100 / documentHeight;
-      asideDelimetrNode.style.setProperty('--top', scrolledFromTop + '%');
+        var scrolledFromTop = getScrolledTop() * 100 / documentHeight;
+        asideDelimetrNode.style.setProperty('--top', scrolledFromTop + '%');
     });
-  } else {
-    console.log('Sorry, your browser is not support this stuff');
-    asideNode.remove();
-  }
+
 }());
